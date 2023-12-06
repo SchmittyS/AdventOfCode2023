2910e31475c52d4cc90a556b05af197ec378577d
Schmitty <schmitzds@outlook.com>
2023-12-06 05:16:10 -0600

    Working Part 2

--- Day2/Challenge2.py
+++ Day2/Challenge2.py
@@ -38,20 +38,46 @@ def check_if_valid_game(row):
 
 def get_lowest_cubes_count(row):
     row=row.rstrip('\n')
-
+    line_split=row.split(": ")
+    
+    #Game Values
+    game_values=line_split[1].split("; ")
+    
+    #Itterate through all variables in the round
+    red_cubes=0
+    green_cubes=0
+    blue_cubes=0
+    
+    #Loop through all round values
+    for x in game_values:
+        round=x.split(", ")
+        
+        #Loop through all the cubes
+        for value in round:  
+            color_value=value.split(" ")
+            
+            #Get the color and check if greater than the last highest cube count
+            #Change if greater than the highest
+            if(color_value[1]=="blue" and int(color_value[0])>blue_cubes):
+                blue_cubes=int(color_value[0])
+            elif(color_value[1]=="green"and int(color_value[0])>green_cubes):
+                green_cubes=int(color_value[0])
+            elif(color_value[1]=="red"and int(color_value[0])>red_cubes):
+                red_cubes=int(color_value[0])
+        
+    return red_cubes*green_cubes*blue_cubes
     
 def readFile(fileLocation):
     game_counter=0
+    game2_counter=0
     with open(fileLocation, 'r') as fileobj:
         for row in fileobj:
             game_counter=game_counter+check_if_valid_game(row)
-            
+            game2_counter=game2_counter+get_lowest_cubes_count(row)
+    
     print("Part 1: "+str(game_counter))
-
-
-
+    print("Part 2: "+str(game2_counter))
 
 if __name__ == '__main__':
-    
     readFile("input1.txt")
     
