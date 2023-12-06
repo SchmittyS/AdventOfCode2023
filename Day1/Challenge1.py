e66c13b16ebe6e57eb65554894af874224926c03
Schmitty <schmitzds@outlook.com>
2023-12-05 18:03:27 -0600

    Added Part 2 of the Challenge

--- Day1/Challenge1.py
+++ Day1/Challenge1.py
@@ -1,9 +1,11 @@
-testString0 = "1aaaa3"
-testString1="ssd3esdsa34"
-
+import re
 
 def part1(sum):
     sum=loopThroughList("input1.txt",sum)
+    return sum
+
+def part2(sum):
+    sum=loopThroughList2("input1.txt",sum)
     return sum
 
 def listToSingleNumber(intList):
@@ -17,6 +19,15 @@ def loopThroughList(fileLocation, addedSum):
             addedSum=addedSum+listToSingleNumber(listInts)
     return addedSum
 
+def loopThroughList2(fileLocation, addedSum):
+    with open(fileLocation, 'r') as fileobj:
+        for row in fileobj:
+            row=row.rstrip('\n')
+            listInts=getintList2(row)
+            singleNumber=listToSingleNumber(listInts)
+            addedSum=addedSum+singleNumber
+
+    return addedSum
 
 def getintList(stringToTest):
     isFirstNumber=True
@@ -35,7 +46,37 @@ def getintList(stringToTest):
         numberList[1]=numberList[0]
     return numberList
 
+def getintList2(stringToFind):
+    
+    numberList=[0,0]
+    numeric_dict={}
+    strNumber_dict={"one":1,"two":2,"three":3,"four":4,"five":5,"six":6,"seven":7,"eight":8, "nine":9 }
+    
+    #Get the Index of where the ints are and what the value is
+    for index,char in enumerate(stringToFind):
+        if char.isnumeric():
+            numeric_dict[index]=int(char)
+    
+    #Get were all the word index are and their int value
+    for key in strNumber_dict:
+        for string in re.finditer(key,stringToFind):
+            numeric_dict[string.start()]=strNumber_dict[key]
+    
+    #Sort the numeric dictionary by index
+    numeric_keys=sorted(list(numeric_dict))
+    
+    #Get the first and last index values    
+    numberList[0]=numeric_dict[numeric_keys[0]]
+    numberList[1]=numeric_dict[numeric_keys[-1]]
+    
+    return numberList   
+
+
 if __name__ == '__main__':
     sum=0
     sum=part1(sum)
+    print(sum)
+    
+    sum=0
+    sum=part2(sum)
     print(sum)
